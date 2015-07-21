ad8b99515eefbb98f95aef4a84f08d9fd6608544
Tomas Votruba <tomas.vot@gmail.com>
2015-07-21 17:10:46 +0200

    file docs added

--- Exception/MultipleValuesException.php
+++ Exception/MultipleValuesException.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * This file is part of the AutowiringBundle.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Skrz\Bundle\AutowiringBundle\Exception;
 
 use LogicException;
